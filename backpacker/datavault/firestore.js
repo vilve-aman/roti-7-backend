f9e3a2aa154787e96cc5a9edaecb531ca6dc9f76
Aman@Legion5i <vilve.aman@gmail.com>
2023-09-20 00:36:36 +0530

    firebaseconfig updated

--- backpacker/datavault/firestore.js
+++ backpacker/datavault/firestore.js
@@ -4,15 +4,26 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC-gPDBGr2gzBgTWuyYtPA-h-yCDOxijJ4",
+//   authDomain: "roti-drivers.firebaseapp.com",
+//   projectId: "roti-drivers",
+//   storageBucket: "roti-drivers.appspot.com",
+//   messagingSenderId: "452224600882",
+//   appId: "1:452224600882:web:38d6c61a4c3d977633652b",
+//   measurementId: "G-Z0V7F1ESMZ", 
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC-gPDBGr2gzBgTWuyYtPA-h-yCDOxijJ4",
-  authDomain: "roti-drivers.firebaseapp.com",
-  projectId: "roti-drivers",
-  storageBucket: "roti-drivers.appspot.com",
-  messagingSenderId: "452224600882",
-  appId: "1:452224600882:web:38d6c61a4c3d977633652b",
-  measurementId: "G-Z0V7F1ESMZ", 
-};
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
